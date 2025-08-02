6c491a73f13b0fc85401b0e7c7ec1920b88123d4
HuyGiaJerry <huyfin2004@gmail.com>
2025-08-02 22:09:45 +0700

    Add fe (header,footer,order,orderConfirm,login,rsgister,detailProduct (Vd: 43-48))

--- shopapp-fe/src/app/app.module.ts
+++ shopapp-fe/src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'products/:id', component: DetailProductComponent },
+  { path: 'orders', component: OrderComponent },
+  { path: 'orders/:id', component: OrderConfirmComponent }
+];
 
 @NgModule({
   declarations: [
-    
-  
-    HomeComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    DetailProductComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
